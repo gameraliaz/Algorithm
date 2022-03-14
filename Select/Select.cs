890c8293d0bb572ee246776ae44b7a4df03171b7
gameraliaz <gameraliaz@gmail.com>
2022-03-14 15:01:45 +0330

    Add project files.

---
+++ Select/Select.cs
@@ -0,0 +1,55 @@
+﻿namespace Select
+{
+    public static class Quick
+    {
+        public static T QuickSelect<T>(List<T> A, int n, int k)
+        {
+            if (n < 2)
+                return A[0];
+            int max = 0;
+            for (int i = 1; i < n; i++)
+                if (Convert.ToInt32(A[i]) > Convert.ToInt32(A[max]))
+                    max = i;
+            T temp;
+            temp = A[n - 1];
+            A[n - 1] = A[max];
+            A[max] = temp;
+            return QuickSelect(A, 0, n - 2, k);
+        }
+        private static T QuickSelect<T>(List<T> A, int first, int last, int k)
+        {
+            int lower = first + 1, upper = last;
+            T temp;
+            temp = A[first];
+            A[first] = A[(first + last) / 2];
+            A[(first + last) / 2] = temp;
+            T bound = A[first];
+            while (lower <= upper)
+            {
+                while (Convert.ToInt32(A[lower]) < Convert.ToInt32(bound))
+                    lower++;
+                while (Convert.ToInt32(bound) < Convert.ToInt32(A[upper]))
+                    upper--;
+                if (lower < upper)
+                {
+                    temp = A[lower];
+                    A[lower] = A[upper];
+                    A[upper] = temp;
+                    lower++;
+                    upper--;
+                }
+                else lower++;
+            }
+            temp = A[upper];
+            A[upper] = A[first];
+            A[first] = temp;
+            if (upper == k)
+                return A[upper];
+            else if (k < upper)
+                return QuickSelect(A, first, upper - 1, k);
+            else
+                return QuickSelect(A, upper + 1, last, k);
+        }
+    }
+
+}
---
+++ Ui/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Ui.Models;
+
+namespace Ui
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<Number> input { get; set; }=new List<Number>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            List<double> inp = new List<double>() { 30, 40, 21, 421, 213, 22, 214, 234, 23, 1, 2, 4, 6, 25, 128, 20, 223, 412 };
+            int i = 0;
+            foreach (double d in inp)
+            {
+                Number temp = new Number();
+                temp.value = d;
+                temp.index = i;
+                input.Add(temp);
+                i++;
+            }
+            DGrid.ItemsSource = input;
+
+        }
+
+        private void Clear_Click(object sender, RoutedEventArgs e)
+        {
+            input.Clear();
+            DGrid.ItemsSource = null;
+            DGrid.ItemsSource = input;
+        }
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+            Number temp = new Number();
+            temp.value = Convert.ToDouble(NumberArray.Text);
+            temp.index = input.Count;
+            input.Add(temp);
+            DGrid.ItemsSource = null;
+            DGrid.ItemsSource = input;
+        }
+        private void Show_Click(object sender, RoutedEventArgs e)
+        {
+            List<double> inp = new List<double>();
+            foreach (Number n in input)
+                inp.Add(n.value);
+            Awnser.Text = Convert.ToString(Select.Quick.QuickSelect(inp, input.Count, Convert.ToInt32(Ktext.Text)));
+            DGrid.SelectedItem = inp;
+        }
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Grid_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+    }
+}
